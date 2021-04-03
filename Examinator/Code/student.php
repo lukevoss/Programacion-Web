70f275a44f1155beaef5058b218a2b475b420403
Henri <henri.smidt@t-online.de>
2021-04-03 12:50:07 +0200

    student doooone

--- Examinator/Code/student.php
+++ Examinator/Code/student.php
@@ -4,31 +4,35 @@
 <body>
     <h1>Course Overview!</h1>
     <section class = "select-asig-form">
-        <h2>Your courses</h2>
+        <h2>Your courses:</h2>
         <?php
             require_once 'includes/getAsigStud.inc.php';
             require_once 'includes/functions.inc.php';
-            while($resultRow = mysqli_fetch_row($resultData)){
-                $examRunning = $resultRow[1];
-                $asigName = $resultRow[0];
-                echo "<h3>$asigName</h3>";
-                ?>
-                <form action="viewResultsStudent.php" method="post">
-                    <input type="hidden" name="asig" value=<?php echo $asigName; ?>>
-                    <button type="submit" name="edit">view Results</button>
-                </form>
-                <?php
-                if($examRunning){
+            if($nfilas === 0){
+                echo "<h3>You don't have any courses</h3>";
+            }else{
+                while($resultRow = mysqli_fetch_row($resultData)){
+                    $examRunning = $resultRow[1];
+                    $asigName = $resultRow[0];
+                    echo "<h3>$asigName</h3>";
                     ?>
-                    <form action="exam.php" method="post">
-                        <button type="submit" name="startExam" value = <?php echo $asigName; ?>>start Exam</button>
+                    <form action="viewResultsStudent.php" method="post">
+                        <input type="hidden" name="asig" value=<?php echo $asigName; ?>>
+                        <button type="submit" name="edit">view Results</button>
                     </form>
                     <?php
-                }
-                ?>
-                </div>
-                <?php
-                }           
+                    if($examRunning){
+                        ?>
+                        <form action="exam.php" method="post">
+                            <button type="submit" name="startExam" value = <?php echo $asigName; ?>>start Exam</button>
+                        </form>
+                        <?php
+                    }
+                    ?>
+                    </div>
+                    <?php
+                    } 
+            }          
         if (isset($_GET['error'])){
             echo "<p>Unexpected Error occured!</p>";
         }
